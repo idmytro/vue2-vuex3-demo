ca85f2c800f5b64121b024ee54f6f88c90b1d2bc
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2022-02-24 01:33:19 +0200

    add comments

--- src/store/nonNamespacedModules/brands.js
+++ src/store/nonNamespacedModules/brands.js
@@ -4,11 +4,15 @@ const state = () => ({
 
 const getters = {
   brands: (state) => state.brands,
+
+  /* namespace-prefix example */
   brands_brands: (state) => state.brands,
 };
 
 const mutations = {
   setBrands (state, brands) { state.brands = brands; },
+
+  /* namespace-prefix example */
   brands_setBrands (state, brands) { state.brands = brands; },
 };
 
