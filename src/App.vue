20f288c0b08e7dd9ad422a7d34c5aece85bb1f53
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2022-02-23 16:09:36 +0200

    fixes, and maybe finished

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="app">
     <button type="button" @click="handleButtonClick">Set shops</button>
+    <p>{{ items }}</p>
   </div>
 </template>
 
 <script>
 import { mapState, mapMutations } from 'vuex';
-import { SHOPS_MODULE } from './store/modules/shops';
+import { SHOPS_MODULE } from './store/namespaces';
 
 export default {
   name: 'App',
@@ -14,11 +15,16 @@ export default {
     ...mapState(SHOPS_MODULE, ['items']),
     /* or */
     ...mapState(SHOPS_MODULE, { shops: 'items' }),
+    /* also works, but not recommended */
+    ...mapState(SHOPS_MODULE, {
+      alsoShops: (state) => state.items,
+    }),
   },
   methods: {
     ...mapMutations(SHOPS_MODULE, ['setItems']),
     /* or */
     ...mapMutations(SHOPS_MODULE, { setShops: 'setItems' }),
+
     handleButtonClick () {
       this.setItems([1, 2, 3]);
     },
